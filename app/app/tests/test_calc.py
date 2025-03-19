02fbce0da2c9fa220bf8c6f7c84cd866821f68a7
ehmatthes <peiwei.cao@gmail.com>
2025-03-19 16:48:40 +0800

    '5'

---
+++ app/app/tests/test_calc.py
@@ -0,0 +1,22 @@
+"""Test for the calc module"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5, 6)    
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, -5)
+
+    def test_multiply_numbers(self):
+        res = calc.multiply(5, 6)
+        self.assertEqual(res, 30)
+
+    def test_divide_numbers(self):
+        res = calc.divide(10, 2)
+        self.assertEqual(res, 5)
