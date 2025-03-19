9d8c8dc40769fe36bb0343e8aa4e19becca033fe
ehmatthes <peiwei.cao@gmail.com>
2025-03-19 17:04:52 +0800

    '5'

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,11 +4,14 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
 
+
 def multiply(a, b):
     return a * b
+
 
 def divide(a, b):
     return a / b
--- app/app/tests/test_calc.py
+++ app/app/tests/test_calc.py
@@ -5,7 +5,10 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
+
+
     def test_add_numbers(self):
         res = calc.add(5, 6)    
         self.assertEqual(res, 11)
@@ -14,9 +17,11 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
         self.assertEqual(res, -5)
 
+
     def test_multiply_numbers(self):
         res = calc.multiply(5, 6)
         self.assertEqual(res, 30)
+
 
     def test_divide_numbers(self):
         res = calc.divide(10, 2)
