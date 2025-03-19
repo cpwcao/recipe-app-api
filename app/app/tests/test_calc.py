72a6fecb2daab41c61aab06b8eb5ac867f2f238d
ehmatthes <peiwei.cao@gmail.com>
2025-03-19 17:09:55 +0800

    '5'

--- app/app/tests/test_calc.py
+++ app/app/tests/test_calc.py
@@ -8,7 +8,6 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
 
-
     def test_add_numbers(self):
         res = calc.add(5, 6)    
         self.assertEqual(res, 11)
@@ -17,11 +16,9 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
         self.assertEqual(res, -5)
 
-
     def test_multiply_numbers(self):
         res = calc.multiply(5, 6)
         self.assertEqual(res, 30)
-
 
     def test_divide_numbers(self):
         res = calc.divide(10, 2)
