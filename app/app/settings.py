0eb0ec170de420d8618d4a030393d21b1733f125
ehmatthes <peiwei.cao@gmail.com>
2025-03-19 15:40:27 +0800

    '5'

--- app/app/settings.py
+++ app/app/settings.py
@@ -72,11 +72,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
     }
 }
 
